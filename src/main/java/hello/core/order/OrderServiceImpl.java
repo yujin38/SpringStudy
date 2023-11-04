c91580a0b6a962506b0aad97c3232bb0ea794ef0
yujin38 <kuj020308@gmail.com>
2023-11-04 19:59:42 +0900

    lombof - @RequiredArgsConstructor

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,41 +5,16 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository; // final 필수 값을 넣어야 됨
     private final DiscountPolicy discountPolicy; // 필드 주입 권장x 테스트 코드에서 사용
-
-//    // 수정 주입(settr 주입), 선택적으로 의존관계 주입
-//    @Autowired(required = false)
-//    public void setMemberRepository(MemberRepository memberRepository) {
-//        System.out.println("memberRepository = " + memberRepository);
-//        this.memberRepository = memberRepository;
-//    }
-//
-//    @Autowired
-//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-//        System.out.println("discountPolicy = " + discountPolicy);
-//        this.discountPolicy = discountPolicy;
-//    }
-//
-    // 생성자 하나 있으면 @Autowired 생략 가능
-    //@Autowired // 딱 한번만 호출, 불변 값을 바꾸면 안됨
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        System.out.println("1. OrderServiceImpl.OrderServiceImpl");
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-
-//    @Autowired // 일반 메서드 주입. 잘 사용하지 않음
-//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
